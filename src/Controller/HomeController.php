6ba58052988b6c7da47710fdcbb4de9b3d62d769
Benjamin Lemin <benjamin.lemin@gmail.com>
2026-05-02 11:19:50 +0200

    HomeController : route de redirection ajoutée pour géré le switch de langue correctement; base.html.twig : petite correction - j'ai retiré le lang-switcher du top de la page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,19 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
-{
+class HomeController extends AbstractController {
+    #[Route(path: '/', name: 'app_home_redirect')]
+    public function redirectToDefaultLocale(): RedirectResponse {
+        return $this->redirectToRoute('app_home', [
+            '_locale' => 'fr',
+        ]);
+    }
+
     #[Route(
         path: '/{_locale}',
         name: 'app_home',
-        requirements: ['_locale' => 'fr|en'],
-        defaults: ['_locale' => 'fr']
+        requirements: ['_locale' => 'fr|en']
     )]
-    public function index(): Response
-    {
+    public function index(): Response {
         return $this->render('home/index.html.twig');
     }
 }

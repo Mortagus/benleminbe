565540125b886dcaa7b2fa8f7ad21b32f81b35fc
Benjamin Lemin <benjamin.lemin@gmail.com>
2026-05-04 11:24:12 +0200

    Projects: complétion de la liste des projets; correction des liens dans la landing-page; modification de la structure des clés de traductions pour plus de flexibilité; les projets raidgbs, sogesa, publifund et easy4pro sont traduits depuis le fichier markdown

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Yaml\Yaml;
 
 #[Route('/{_locale}/projects', name: 'app_projects_', requirements: ['_locale' => 'fr|en'])]
 final class ProjectsController extends AbstractController
@@ -13,15 +15,53 @@ final class ProjectsController extends AbstractController
 
     private const array PROJECTS = [
         [
+            'key' => 'raidgbs',
+        ],
+        [
+            'key' => 'sogesa',
+        ],
+        [
+            'key' => 'publifund',
+        ],
+        [
+            'key' => 'easy4pro',
+        ],
+        [
+            'key' => 'logic_immo',
+        ],
+        [
+            'key' => 'isobar',
+        ],
+        [
             'key' => 'delcampe',
+        ],
+        [
+            'key' => 'keytrade',
+        ],
+        [
+            'key' => 'famille_chretienne',
         ],
         [
             'key' => 'stanhome',
         ],
         [
+            'key' => 'his',
+        ],
+        [
             'key' => 'moveit',
         ],
+        [
+            'key' => 'marge_delhaize',
+        ],
+        [
+            'key' => 'coaching',
+        ],
     ];
+
+    public function __construct(
+        private readonly KernelInterface $kernel,
+    ) {
+    }
 
     #[Route('', name: 'index')]
     public function index(): Response
@@ -32,12 +72,13 @@ final class ProjectsController extends AbstractController
     }
 
     #[Route('/{project}', name: 'show')]
-    public function show(string $project): Response
+    public function show(string $project, string $_locale): Response
     {
         $projectIndex = $this->findProjectIndex($project);
 
         return $this->render(self::TEMPLATE_DIR . 'detailed_project.html.twig', [
             'project' => $project,
+            'projectData' => $this->getProjectData($project, $_locale),
             'previousProject' => $this->getAdjacentProject($projectIndex, -1),
             'nextProject' => $this->getAdjacentProject($projectIndex, 1),
         ]);
@@ -52,6 +93,30 @@ final class ProjectsController extends AbstractController
         }
 
         throw $this->createNotFoundException(sprintf('Project "%s" not found.', $project));
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    private function getProjectData(string $project, string $locale): array
+    {
+        $translationFile = sprintf(
+            '%s/translations/projects.%s.yaml',
+            $this->kernel->getProjectDir(),
+            $locale,
+        );
+
+        if (!is_file($translationFile)) {
+            throw $this->createNotFoundException(sprintf('Project translation file "%s" not found.', $translationFile));
+        }
+
+        $translations = Yaml::parseFile($translationFile);
+
+        if (!is_array($translations) || !isset($translations[$project]) || !is_array($translations[$project])) {
+            throw $this->createNotFoundException(sprintf('Project translation data "%s" not found.', $project));
+        }
+
+        return $translations[$project];
     }
 
     /**

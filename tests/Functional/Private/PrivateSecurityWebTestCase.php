d578ee628aa3d50cd6230e4a91fcc4dc76222804
Benjamin Lemin <benjamin.lemin@gmail.com>
2026-06-29 11:14:46 +0200

    Improve private test DB failure reporting

--- tests/Functional/Private/PrivateSecurityWebTestCase.php
+++ tests/Functional/Private/PrivateSecurityWebTestCase.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\Private;
 
+use Doctrine\DBAL\Connection;
+use RuntimeException;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -14,12 +16,31 @@ abstract class PrivateSecurityWebTestCase extends WebTestCase
         self::bootKernel();
 
         $connection = self::getContainer()->get('doctrine')->getConnection();
+        $this->assertTestDatabaseIsReachable($connection);
         $connection->executeStatement('SET FOREIGN_KEY_CHECKS=0');
         $connection->executeStatement('TRUNCATE TABLE private_passkey_credentials');
         $connection->executeStatement('SET FOREIGN_KEY_CHECKS=1');
 
         self::getContainer()->get('cache.rate_limiter')->clear();
         self::ensureKernelShutdown();
+    }
+
+    private function assertTestDatabaseIsReachable(Connection $connection): void
+    {
+        try {
+            $connection->executeQuery('SELECT 1');
+        } catch (\Throwable $throwable) {
+            throw new RuntimeException(
+                "La base de donnees de test est inaccessible pour PrivateSecurityWebTest.\n"
+                . "Commandes de recuperation:\n"
+                . "  docker compose stop db\n"
+                . "  docker compose up -d db\n"
+                . "  php bin/phpunit tests/Functional/Private/PrivateSecurityWebTest.php\n"
+                . 'Cause initiale: ' . $throwable->getMessage(),
+                0,
+                $throwable,
+            );
+        }
     }
 
     protected function createAuthenticatedClient(): KernelBrowser
